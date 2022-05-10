824be02af9fc294058c00e7615ddce290e53dc38
KumaTea <KumaTea@outlook.com>
2022-05-10 17:58:24 +0800

    add data and mirrors

---
+++ data.py
@@ -0,0 +1,49 @@
+# Test items
+test_items = {
+    'alpine': {
+        'name': 'Alpine',
+        'path': 'alpine'
+    },
+    'archlinux': {
+        'name': 'ArchLinux',
+        'path': 'archlinux'
+    },
+    'centos': {
+        'name': 'CentOS',
+        'path': 'centos'
+    },
+    'debian': {
+        'name': 'Debian',
+        'path': 'debian'
+    },
+    'epel': {
+        'name': 'EPEL',  # Extra Packages for Enterprise Linux
+        'path': 'epel'
+    },
+    'fedora': {
+        'name': 'Fedora',
+        'path': 'fedora'
+    },
+    'kernel': {
+        'name': 'Kernel',  # Linux Kernel
+        'path': 'kernel'
+    },
+    'openwrt': {
+        'name': 'OpenWrt',
+        'path': 'openwrt'
+    },
+    'pypi': {
+        'name': 'PyPI',  # Python Package Index
+        'path': 'pypi'
+    },
+    'ubuntu': {
+        'name': 'Ubuntu',
+        'path': 'ubuntu'
+    }
+}
+
+large_files = {
+}
+
+tiny_files = {
+}
---
+++ mirrors.py
@@ -0,0 +1,124 @@
+# Public Mirrors
+
+# Universities
+universities = {
+    'bfsu': {
+        'name': '北京外国语大学',
+        'url': 'https://mirrors.bfsu.edu.cn'
+    },
+    'bit': {
+        'name': '北京理工大学',
+        'url': 'https://mirror.bit.edu.cn'
+    },
+    'bjtu': {
+        'name': '北京交通大学',
+        'url': 'https://mirror.bjtu.edu.cn'
+    },
+    'bupt': {
+        'name': '北京邮电大学',
+        'url': 'https://mirrors.bupt.edu.cn'
+    },
+    'cqu': {
+        'name': '重庆大学',
+        'url': 'https://mirrors.cqu.edu.cn'
+    },
+    'cqupt': {
+        'name': '重庆邮电大学',
+        'url': 'https://mirrors.cqupt.edu.cn'
+    },
+    'dgut': {
+        'name': '东莞理工学院',
+        'url': 'https://mirrors.dgut.edu.cn'
+    },
+    'hit': {
+        'name': '哈尔滨工业大学',
+        'url': 'https://mirrors.hit.edu.cn'
+    },
+    'lzu': {
+        'name': '兰州大学',
+        'url': 'https://mirror.lzu.edu.cn'
+    },
+    'neusoft': {
+        'name': '大连东软信息学院',
+        'url': 'https://mirrors.neusoft.edu.cn'
+    },
+    'nju': {
+        'name': 'NJU Mirror',  # 南京大学
+        'url': 'https://mirrors.nju.edu.cn'
+    },
+    'njupt': {
+        'name': '南京邮电大学',
+        'url': 'https://mirrors.njupt.edu.cn'
+    },
+    'nwafu': {
+        'name': '西北农林科技大学',
+        'url': 'https://mirrors.nwafu.edu.cn'
+    },
+    'shanghaitech': {
+        'name': 'Geekpie',
+        'url': 'https://mirrors.shanghaitech.edu.cn'
+    },
+    'sjtu': {
+        'name': 'sjtu',  # 上海交通大学,
+        'url': 'https://mirror.sjtu.edu.cn'
+    },
+    'sjtug': {
+        'name': 'sjtug',  # 上海交通大学,
+        'url': 'https://mirrors.sjtug.sjtu.edu.cn'
+    },
+    'tuna': {
+        'name': 'TUNA',  # 清华大学
+        'url': 'https://mirrors.tuna.tsinghua.edu.cn'
+    },
+    'ustc': {
+        'name': 'USTC',  # 中国科学技术大学
+        'url': 'https://mirrors.ustc.edu.cn'
+    },
+    'xjtu': {
+        'name': '西安交通大学',
+        'url': 'https://mirrors.xjtu.edu.cn'
+    }
+}
+
+commercials = {
+    'aliyun': {
+        'name': '阿里',
+        'url': 'https://mirrors.aliyun.com'
+    },
+    'tencent': {
+        'name': '腾讯',
+        'url': 'https://mirrors.tencent.com'
+    },
+    'huawei': {
+        'name': '华为',
+        'url': 'https://repo.huaweicloud.com'
+    },
+    'cn99': {
+        'name': 'CN99',
+        'url': 'https://mirrors.cn99.com'
+    },
+    '163': {
+        'name': '网易',
+        'url': 'https://mirrors.163.com'
+    },
+    'cnnic': {
+        'name': 'CNNIC',
+        'url': 'https://mirrors.cnnic.cn'
+    },
+    'sohu': {
+        'name': '搜狐',
+        'url': 'https://mirrors.sohu.com'
+    },
+    'yun-idc': {
+        'name': '首都在线',
+        'url': 'https://mirrors.yun-idc.com'
+    },
+}
+
+# Dismissed mirrors
+# 同步镜像过少
+# 不向公网开放
+dismissed = [
+    'dlut'
+    'hust'
+]
